228fd3ead0226bfaeecd8eb41d8934b921a0e3bd
Rajdeep De <surjode2003@gmail.com>
2023-12-17 01:01:32 +0530

    header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 
 export default function Home() {
   return (
     <main className="">
       <h1>Data Port</h1>
+
+      <UserButton/>
     </main>
   )
 }
---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+function Header() {
+  return (
+    <header className="flex items-center justify-between">
+        <Link>
+         
+        </Link>
+    </header>
+  )
+}
+
+export default Header
